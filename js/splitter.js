e2c0fd75b929226bc41a8c0965f0569ee7348282
shidqiiii <38581836+shidqiiii@users.noreply.github.com>
2022-05-13 22:24:00 +0700

    separate function

--- js/splitter.js
+++ js/splitter.js
@@ -1,49 +1,62 @@
-var bill = document.getElementById("input-bill");
-var tip = document.querySelectorAll(".tip-amount");
-var people = document.getElementById("input-people");
-var reset = document.getElementById("reset");
-let tipamount = document.getElementById("tipperson");
-let totalamount = document.getElementById("totalperperson");
+let bill = document.getElementById("input-bill");
+let tip = document.querySelectorAll(".tip-amount");
+let people = document.getElementById("input-people");
+let reset = document.getElementById("reset");
+let tipamount = document.getElementById("tip-person");
+let totalamount = document.getElementById("total-person");
 let customtip = document.getElementById("input-tip");
 
+let tipMuch = "";
+
 bill.addEventListener('keyup', number)
-people.addEventListener('keyup', number)
+
+customtip.addEventListener('keyup', function () {
+    tipMuch = customtip.value
+    tip.forEach(element => {
+        element.classList.remove("selected");
+    })
+    SplitTip(parseFloat(bill.value), parseFloat(tipMuch), parseInt(people.value));
+})
+
+people.addEventListener('keyup', function () {
+    number();
+    SplitTip(parseFloat(bill.value), parseFloat(tipMuch), parseInt(people.value));
+})
 
 function number() {
-    if (people.value === "" || bill.value === "") {
+    if (parseInt(people.value) === "" || parseFloat(bill.value) === "") {
         reset.disabled = true
     }
-    if (people.value === "0") {
+    if (parseInt(people.value) === 0) {
         document.getElementById("error-message").style.display = "block";
         document.getElementsByClassName("input-value")[1].style.border = "1px solid rgb(198, 45, 14)";
     }
     else {
         reset.disabled = false
         document.getElementById("error-message").style.display = "none";
-        document.getElementsByClassName("input-value")[1].style.border = "1px solid $very-light-grayish-cyan";
+        document.getElementsByClassName("input-value")[1].style.border = "1px solid hsl(189, 41%, 97%)";
     }
+}
+
+function SplitTip(billAmount, tipAmount, personAmount) {
+    let tipPerPerson = (billAmount * tipAmount / 100) / personAmount
+    let totalPerPerson = (billAmount / personAmount) + tipPerPerson
+
+    tipamount.innerHTML = "$" + tipPerPerson.toFixed(2).toString();
+    totalamount.innerHTML = "$" + totalPerPerson.toFixed(2).toString();
 }
 
 tip.forEach(tips => {
     tips.addEventListener('click', function () {
-        console.log(tips.id)
+        tipMuch = tips.id
+        customtip.value = ""
 
         tip.forEach(element => {
             element.classList.remove("selected");
-
             this.classList.add("selected")
         })
 
-        let tipPerPerson = (parseFloat(bill.value) * parseFloat(tips.id) / 100) / people.value
-        let totalPerPerson = (parseFloat(bill.value) / people.value) + tipPerPerson
-
-        if (bill.value == "" || people.value == "") {
-            tipamount.innerHTML = "$0.00"
-            totalamount.innerHTML = "$0.00"
-        } else {
-            tipamount.innerHTML = "$" + tipPerPerson.toFixed(2).toString();
-            totalamount.innerHTML = "$" + totalPerPerson.toFixed(2).toString();
-        }
+        SplitTip(parseFloat(bill.value), parseFloat(tipMuch), parseInt(people.value));
     })
 });
 
@@ -53,4 +66,7 @@ reset.addEventListener('click', function () {
     tipamount.innerHTML = "$0.00"
     totalamount.innerHTML = "$0.00"
     reset.disabled = true
+    tip.forEach(element => {
+        element.classList.remove("selected");
+    })
 })
